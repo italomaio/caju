022b3769da50559f26a715b7d0989bf34fa74731
Developer <italomaio@gmail.com>
2024-11-22 17:50:17 -0300

    [fix] - added vite vars to process

--- src/configs/axios.ts
+++ src/configs/axios.ts
@@ -1,5 +1,6 @@
 import axios from "axios";
 
+console.log("API_URL_INSTANCE", import.meta?.env);
 export const axiosInstance = axios.create({
-  baseURL: import.meta?.env?.VITE_API_URL,
+  baseURL: import.meta?.env?.VITE_API_URL || "http://localhost:3000",
 });
