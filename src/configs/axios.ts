5ff45a5055d0dc4ac40091e51d8b396a8eae4591
Developer <italomaio@gmail.com>
2024-11-22 15:01:11 -0300

    [fix] vars

--- src/configs/axios.ts
+++ src/configs/axios.ts
@@ -1,8 +1,5 @@
 import axios from "axios";
 
 export const axiosInstance = axios.create({
-  baseURL:
-    process.env.API_URL ||
-    import.meta.env.VITE_APP_API_URL ||
-    "http://localhost:3000",
+  baseURL: process.env.API_URL || "http://localhost:3000",
 });
